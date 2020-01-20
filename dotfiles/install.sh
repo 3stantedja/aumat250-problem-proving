9d76927efebd3014f1ef463d0fbaf95c067344fa
Tristan <tristan.tedja@me.com>
2020-01-20 03:42:52 -0700

    more edits to install script

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 TEXLIVE="http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz"
 TEXLIVEFILE="install-tl-unx.tar.gz"
+lPATHS="texlive.sh"
 ROOTDIR=/usr/local/texlive/2019
 USERDIR=$HOME/.texlive2019
 
 export PATH=/usr/local/texlive/2019/bin/x86_64-linux:$PATH 
 
 # Clean up previous installations if it fails previously
-rm -rf $ROOTDIR
-rm -rf $USERDIR
+sudo rm -rf $ROOTDIR
+sudo rm -rf $USERDIR
 
-# Download a latest copy of TeX Live and extract to "install-tl"
-wget $TEXLIVE && tar -xzvf $TEXLIVEFILE -C install-tl
- 
-cd install-tl
-sudo ./install-tl --profile=texlive.profile
+# Download a latest copy of TeX Live
+wget $TEXLIVE 
+tar -xzvf $TEXLIVEFILE
 
-tlmgr update --self
+# Start installation with provided profile
+cd install-tl-20*
+sudo ./install-tl --profile=../texlive.profile
+
+sudo cp $lPATH /etc/profile.d/$lPATH && source /etc/profile.d/$lPATH
+sudo env PATH="$PATH" tlmgr path add
+
+sudo tlmgr update --self
